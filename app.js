6cb173a24f073c14ba0fe8dcd3185e20c2be6b27
subanah <162430575+subanah@users.noreply.github.com>
2024-06-11 09:14:34 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('.slick-parent').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrow: true,
+        centerMode: true,
+        centerPadding: '40px',
+        prevArrow:'<span class="left"><i class="fa-solid fa-caret-left"></i></span>',
+        nextArrow:'<span class="right"><i class="fa-solid fa-caret-right"></i></span>'
+      });
+
+    
+})
